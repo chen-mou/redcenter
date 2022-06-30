c5d36410ddf357d9c329a03073503d36648ce9bd
陈桢梁 <chenzhenliang@xxpie.com>
2022-06-30 16:32:41 +0800

    完成了协议头部的数据获取以及校验

--- main/client/redcenter/protool/entity.go
+++ main/client/redcenter/protool/entity.go
@@ -1,20 +1,19 @@
 package protool
 
 import (
-	"net"
 	"sync"
 )
 
 type Head struct {
-	Custom   map[string][]string
-	Auth     string
-	locks    map[string]sync.Mutex `json:"-"`
-	isClose  bool                  `json:"-"`
-	net.Conn `json:"-"`
+	Custom  map[string][]string
+	Auth    string
+	Length  int
+	locks   map[string]sync.Mutex `json:"-"`
+	isClose bool                  `json:"-"`
 }
 
 type Body struct {
-	isClose  bool                  `json:"-"`
-	locks    map[string]sync.Mutex `json:"-"`
-	net.Conn `json:"-"`
+	isClose bool                  `json:"-"`
+	locks   map[string]sync.Mutex `json:"-"`
+	Data    interface{}
 }
--- main/client/redcenter/redcenter_server.go
+++ main/client/redcenter/redcenter_server.go
@@ -1,9 +1,14 @@
 package redcenter
 
 import (
+	"RedisRegister/main/client/redcenter/protool"
 	"RedisRegister/main/client/redis"
 	"RedisRegister/tool/log"
+	"encoding/json"
+	"errors"
 	"net"
+	"strconv"
+	"strings"
 )
 
 type Configure struct {
@@ -23,6 +28,7 @@ type Server struct {
 	conf   *Configure
 	wconf  *WebConfig
 	logger log.Logger
+	auth   func(string) (bool, string)
 }
 
 func Open(conf *Configure, wconf *WebConfig) (*Server, error) {
@@ -33,7 +39,90 @@ func Open(conf *Configure, wconf *WebConfig) (*Server, error) {
 	return s, nil
 }
 
-func handler(conn net.Conn) {}
+func Error(msg string) []byte {
+	return []byte("&code:500-&errmsg=" + msg + "-")
+}
+
+func Successful(msg string, data interface{}) []byte {
+	begin := "&code:200-&{\"msg\":+\"" + msg + "\""
+	end := "}-"
+	if data == nil {
+		return []byte(begin + end)
+	}
+	jn, _ := json.Marshal(data)
+	b := []byte(begin + ",\"data\":" + string(jn) + end)
+	return b
+}
+
+func Fail(code string, msg string) []byte {
+	return []byte("&code:" + code + "-&{\"msg\":\"" + msg + "\"}-")
+}
+
+/*
+& 开始符表示数据开始
+- 结束符表示数据结束
+\ 转义符表示下个字符为普通字符
+*/
+func (s *Server) handler(conn net.Conn) {
+	head := make([]byte, 256)
+	conn.Read(head)
+	defer conn.Close()
+	hd, err := headHandler(head)
+	if err != nil {
+		conn.Write(Error(err.Error()))
+		return
+	}
+	if ok, msg := s.auth(hd.Auth); !ok {
+		conn.Write(Fail("403", msg))
+		return
+	} else {
+		conn.Write(Successful("ok", nil))
+	}
+}
+
+func headHandler(head []byte) (*protool.Head, error) {
+	data := make([]byte, 1)
+	start := false
+	closed := false
+	for i := 0; i < len(head) && !closed; i++ {
+		if head[i] == '&' {
+			start = true
+			continue
+		}
+		if !start {
+			continue
+		}
+		switch head[i] {
+		case '-':
+			closed = true
+		case '\\':
+			data = append(data, head[i])
+			i++
+		default:
+			data = append(data, head[i])
+		}
+	}
+	h := &protool.Head{
+		Custom: map[string][]string{},
+	}
+	atrs := strings.Split(string(data), "\n")
+	for _, value := range atrs {
+		names := strings.Split(value, ":")
+		switch names[1] {
+		case "Auth", "auth", "AUTH":
+			h.Auth = names[2]
+		case "Content-Length", "content-length":
+			var err error
+			h.Length, err = strconv.Atoi(names[2])
+			if err != nil {
+				return nil, errors.New("头属性Content-Length有误")
+			}
+		default:
+			h.Custom[names[0]] = strings.Split(names[1], ",")
+		}
+	}
+	return h, nil
+}
 
 func (s Server) Run() error {
 	s.logger.Info("准备连接redis")
